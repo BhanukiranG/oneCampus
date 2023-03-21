db1ecba94bfce51f1c9b9daba99e2ae705b5d7fe
BhanuKiranG <bhanukiran5102@gmail.com>
2023-03-21 20:21:20 +0530

    upload

--- app/src/main/java/com/example/onecampus/eventsUpload.java
+++ app/src/main/java/com/example/onecampus/eventsUpload.java
@@ -5,11 +5,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.karumi.dexter.Dexter;
@@ -20,28 +22,36 @@ import com.karumi.dexter.listener.PermissionRequest;
 import com.karumi.dexter.listener.single.PermissionListener;
 
 public class eventsUpload extends AppCompatActivity {
-    EditText nameOfEvent,typeOfEvent,discription,Link;
-    ImageView uploadImg,productImg;
+
+    TextView description,title,clg;
+    ImageView uploadbtn,productImage;
+    RelativeLayout relative;
     Button submit;
+    Uri imageuri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_events_upload);
-        nameOfEvent=findViewById(R.id.nameOfEvent);
-        typeOfEvent=findViewById(R.id.typeOfEvent);
-        discription=findViewById(R.id.Discription);
-        Link=findViewById(R.id.LinkOfEvent);
-        uploadImg=findViewById(R.id.uploadImage);
-        productImg=findViewById(R.id.productImage);
-        submit=findViewById(R.id.uploadFireBase);
-        uploadImg.setOnClickListener(new View.OnClickListener() {
+        description=findViewById(R.id.description);
+        title=findViewById(R.id.title);
+        clg=findViewById(R.id.clg);
+        uploadbtn=findViewById(R.id.uploadbtn);
+        relative=findViewById(R.id.relative);
+        submit=findViewById(R.id.submit);
+        productImage=findViewById(R.id.productImage);
+
+        uploadbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 uploadImage();
+                relative.setVisibility(View.VISIBLE);
+                uploadbtn.setVisibility(View.GONE);
             }
         });
     }
-    private void uploadImage(){
+
+    private void uploadImage() {
         Dexter.withContext(this)
                 .withPermission(Manifest.permission.READ_EXTERNAL_STORAGE)
                 .withListener(new PermissionListener() {
@@ -55,7 +65,7 @@ public class eventsUpload extends AppCompatActivity {
 
                     @Override
                     public void onPermissionDenied(PermissionDeniedResponse permissionDeniedResponse) {
-                        Toast.makeText(eventsUpload.this, "permission denied", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(eventsUpload.this,"permission denied",Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -68,6 +78,9 @@ public class eventsUpload extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if(requestCode==101)
+        if(requestCode==101 && resultCode==RESULT_OK){
+            imageuri=data.getData();
+            productImage.setImageURI(imageuri);
+        }
     }
 }
