c8fcc7667859b777bb0a51b4b67c058f3e45f86a
nani2912 <guntuku001@gmail.com>
2023-03-24 10:51:00 +0530

    Forgot Passsword Implemented

--- app/src/main/java/com/example/onecampus/userLogin.java
+++ app/src/main/java/com/example/onecampus/userLogin.java
@@ -1,18 +1,25 @@
 package com.example.onecampus;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -22,6 +29,7 @@ public class userLogin extends AppCompatActivity {
     EditText editTextEmail,editTextPassword;
     Button userSigin;
     FirebaseAuth mAuth;
+    TextView forgetpass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,6 +38,13 @@ public class userLogin extends AppCompatActivity {
         editTextEmail=findViewById(R.id.userEmailLogin);
         editTextPassword=findViewById(R.id.userPasswordLogin);
         userSigin=findViewById(R.id.userSignin);
+        forgetpass=findViewById(R.id.forgetpass);
+        forgetpass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showRecoverProcessDialog();
+            }
+        });
         userSigin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,4 +78,69 @@ public class userLogin extends AppCompatActivity {
             }
         });
     }
-}
+    private void showRecoverProcessDialog() {
+            AlertDialog.Builder builder=new AlertDialog.Builder(this);
+            builder.setTitle("Recover Password");
+            LinearLayout linearLayout=new LinearLayout(this);
+            final EditText emailet= new EditText(this);
+
+            // write the email using which you registered
+            //emailet.setText("Email");
+            emailet.setHint("Email");
+            emailet.setMinEms(16);
+            emailet.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+            linearLayout.addView(emailet);
+            linearLayout.setPadding(10,10,10,10);
+            builder.setView(linearLayout);
+
+            // Click on Recover and a email will be sent to your registered email id
+            builder.setPositiveButton("Recover", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    String email=emailet.getText().toString().trim();
+                    beginRecovery(email);
+                }
+            });
+
+            builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+            builder.create().show();
+        }
+
+        private void beginRecovery(String email) {
+            ProgressDialog loadingBar = new ProgressDialog(this);
+            loadingBar.setMessage("Sending Email....");
+            loadingBar.setCanceledOnTouchOutside(false);
+            loadingBar.show();
+
+            // calling sendPasswordResetEmail
+            // open your email and write the new
+            // password and then you can login
+            mAuth.sendPasswordResetEmail(email).addOnCompleteListener(new OnCompleteListener<Void>() {
+                @Override
+                public void onComplete(@NonNull Task<Void> task) {
+                    loadingBar.dismiss();
+                    if(task.isSuccessful())
+                    {
+                        // if isSuccessful then done message will be shown
+                        // and you can change the password
+                        Toast.makeText(userLogin.this,"Done sent",Toast.LENGTH_LONG).show();
+                    }
+                    else {
+                        Toast.makeText(userLogin.this,"Error Occurred",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }).addOnFailureListener(new OnFailureListener() {
+                @Override
+                public void onFailure(@NonNull Exception e) {
+                    loadingBar.dismiss();
+                    Toast.makeText(userLogin.this,"Error Failed",Toast.LENGTH_LONG).show();
+                }
+            });
+        }
+
+    }
