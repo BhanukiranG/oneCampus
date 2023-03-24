7589cce4bc5189b1a91b2f311a729a5efb177b49
tharu <20211a0595@bvrit.ac.in>
2023-03-24 13:29:24 +0530

    final bt Tharun  user1

---
+++ app/src/main/java/com/example/onecampus/user1.java
@@ -0,0 +1,35 @@
+package com.example.onecampus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+public class user1 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_1);
+        Button logout=(Button) findViewById(R.id.logout);
+        logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences sharedPreferences=getSharedPreferences(userLogin.PREFS_NAME,0);
+                SharedPreferences.Editor editor=sharedPreferences.edit();
+                editor.putBoolean("hasLoggedin",false);
+                editor.commit();
+                FirebaseAuth.getInstance().signOut();
+                Intent intent=new Intent(user1.this,MainActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+}
